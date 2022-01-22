aec7f548a6d8ee7b143463af9040fe04f2c2a90c
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-01-21 18:48:10 -0800

    Update longest-palindromic-substring.py

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -2,21 +2,20 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         
         # O(n^2) time O(1) space
-        # Expand around center
+        # Expand around center 
         
-        def expandAroundCenter(left, right):
+        def expandAroundTheCenter(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                left-=1
-                right+=1
+                left, right = left-1, right+1
             return right-left-1
         
         start, end = 0, 0
         for i in range(len(s)):
-            oddLen = expandAroundCenter(i, i)
-            evenLen = expandAroundCenter(i, i+1)
+            oddLen = expandAroundTheCenter(i, i)
+            evenLen = expandAroundTheCenter(i, i+1)
             maxLen = max(oddLen, evenLen)
             
-            if maxLen > end-start:
+            if end-start < maxLen:
                 start = i - (maxLen-1)//2
                 end = i + (maxLen//2)
                 
