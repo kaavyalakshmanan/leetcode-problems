2c068bd714f56f60d101cbe40b8cb7056659d84f
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-01-21 18:47:27 -0800

    Update valid-parentheses.py

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -2,13 +2,13 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
         # O(n) time O(n) space
-        # Using a stack
+        # Use stack
         
         stack = []
         for c in s:
             if c == "(" or c == "{" or c == "[":
                 stack.append(c)
-            elif not stack or ((c == ")" and stack[-1] != "(") or (c == "}" and stack[-1] != "{") or (c == "]" and stack[-1] != "[")):
+            elif not stack or (c == ")" and stack[-1] != "(") or (c == "}" and stack[-1] != "{") or (c == "]" and stack[-1] != "["):
                 return False
             else:
                 stack.pop()
