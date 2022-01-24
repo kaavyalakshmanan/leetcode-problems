fe01477aefa885a0aac9e460a65ecea3ea67a37f
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-01-23 21:51:19 -0800

    Create implement-trie.py

---
+++ implement-trie.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False   
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+    # O(n) time O(n) space 
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+            
+        
+    # O(n) time O(1) space
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.endOfWord
+        
+    # O(n) time O(1) space
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
