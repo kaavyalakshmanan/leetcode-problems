96fc2d04cdcb18aafc130cced581d24eeef57820
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-02-14 10:07:13 -0800

    Update median-of-two-sorted-arrays.py

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -1,36 +1,31 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
-        # O(log(n)) time O(1) space where n = len(A)
-        # Binary Search
+        # O(log(m+n)) time O(1) space
         
         A, B = nums1, nums2
         if len(B) < len(A):
             A, B = B, A
-            
-        left, right = 0, len(A)-1
         total = len(A) + len(B)
         half = total // 2
+            
+        left, right = 0, len(A)-1 
+        
+        
         while True:
-            midA = (right+left)//2
+            midA = (left + right) // 2
             midB = half - midA - 2
+            leftA, rightA = A[midA] if midA >= 0 else -inf, A[midA+1] if midA+1 < len(A) else inf
+            leftB, rightB = B[midB] if midB >= 0 else -inf, B[midB+1] if midB+1 < len(B) else inf
             
-            leftA = A[midA] if midA >= 0 else -inf
-            leftB = B[midB] if midB >= 0 else -inf
-            rightA = A[midA+1] if midA+1 < len(A) else inf
-            rightB = B[midB+1] if midB+1 < len(B) else inf
-            
-            if leftB <= rightA and leftA <= rightB:
-                if total % 2:
+            if leftA <= rightB and leftB <= rightA:
+                if (total) % 2 == 1:
                     return min(rightA, rightB)
                 else:
-                    return (max(leftA, leftB) + min(rightA, rightB)) / 2
-            elif leftA > rightB:
-                right = midA-1
-            else:
+                    return (max(leftA, leftB) + min(rightA, rightB))/2
+            elif leftB > rightA:
                 left = midA + 1
+            else:
+                right = midA-1
                 
             
-            
-            
-        
