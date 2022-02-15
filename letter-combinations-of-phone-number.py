fd3a47f64563f336fe3fc309dd0bed2f7a9cdd2b
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-02-14 21:34:09 -0800

    Update letter-combinations-of-phone-number.py

--- letter-combinations-of-phone-number.py
+++ letter-combinations-of-phone-number.py
@@ -4,28 +4,29 @@ class Solution:
         # O(n*4^n) time O(n) space
         # Backtracking 
         
-        def backtrack(idx, curr):
-            if idx == len(digits):
-                output.append(curr)
-                return
-            
-            currLetters = letters[digits[idx]]
-            for c in currLetters:
-                backtrack(idx+1, curr+c)
-        
-        letters = {
-            "2": ["a", "b", "c"],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"],
+        digitsToLetters = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z'],
         }
         
+        def backtracking(idx, currLetters):
+            if idx == len(digits):
+                output.append(currLetters)
+                return
+            
+            for c in digitsToLetters[digits[idx]]:
+                backtracking(idx+1, currLetters+c)
+            
         output = []
-        if not digits:
-            return output
-        backtrack(0, "")
-        return output 
+        if digits:
+            backtracking(0, "")
+        return output
+            
+            
+        
