29c62ee344bd43bf9074fd746d8f17a4314cdc23
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-02-14 21:32:49 -0800

    Update regular-expression-matching.py

--- regular-expression-matching.py
+++ regular-expression-matching.py
@@ -1,30 +1,29 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        # O(s*p) time O(s*p) space
-        # Top down memoization
+        # O(m*n) time O(m*n) space
+        # Memoization
         
+        cache = {}
+        
+        # Backtracking algo with memoization
         def findMatch(i, j):
-            # Base cases
+            if (i,j) in cache:
+                return cache[(i,j)]
             if i >= len(s) and j >= len(p):
                 return True
             if j >= len(p):
                 return False
-            if (i,j) in cache:
-                return cache[(i,j)] 
-
+            
             match = i < len(s) and (s[i] == p[j] or p[j] == '.')
             if j+1 < len(p) and p[j+1] == '*':
-                # Use star or don't use star
-                # If we are using star, there has to be a match
-                cache[(i,j)] = (match and findMatch(i+1, j)) or findMatch(i, j+2)
-                return cache[(i,j)] 
+                # Don't use * or use * IF there is match
+                cache[(i,j)] = findMatch(i, j+2) or (match and findMatch(i+1, j))
+                return cache[(i,j)]
             if match: 
                 cache[(i,j)] = findMatch(i+1, j+1)
-                return cache[(i,j)] 
+                return cache[(i,j)]
             else:
-                cache[(i,j)] = False
-                return cache[(i,j)] 
+                return False
             
-        cache = {}
         return findMatch(0, 0)
