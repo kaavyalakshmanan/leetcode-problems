3f668487020d210e2df3d2a8a1c098433ec1c37b
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-02-21 14:21:38 -0800

    Update basic-calculator-ii.py

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -1,26 +1,33 @@
 class Solution:
     def calculate(self, s: str) -> int:
         
-        length = len(s)
+        # O(n) time O(n) space
         stack = []
-        currNum, op = 0, '+'
+        currNum, currChar, op = 0, '', '+'
+        length = len(s)
+        
+        def div(n1, n2):
+            if n1 > 0 or n1 % n2 == 0:
+                return n1 // n2
+            return (n1 // n2) + 1
+                
         
         for i in range(length):
             currChar = s[i]
             if currChar.isdigit():
-                currNum = (currNum * 10) + (currChar - '0')
-            if (not currChar.isDigit() and not currChar.isspace()) or i == length - 1:
-                if op == '-':
-                    stack.push(-currNum)
-                elif op == '+':
-                    stack.push(currNum)
+                currNum = (currNum * 10) + int(currChar)
+            if (not currChar.isdigit() and not currChar.isspace()) or i == length-1:
+                if op == '+':
+                    stack.append(currNum)
+                elif op == '-':
+                    stack.append(-currNum)
                 elif op == '*':
-                    stack.push(stack.pop() * currNum)
-                elif op == '/':
-                    stack.push(stack.pop() / currNum)
-                op = currChar
+                    stack[-1] = stack[-1] * currNum
+                else:
+                    stack[-1] = div(stack[-1], currNum)
                 currNum = 0
-                
+                op = currChar
+        
         res = 0
         while stack:
             res += stack.pop()
