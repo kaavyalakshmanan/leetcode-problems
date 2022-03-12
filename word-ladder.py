c89f67ae85908cd7deec0b21f4d4c5388a629f1b
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-12 11:47:13 -0800

    Create word-ladder.py

---
+++ word-ladder.py
@@ -0,0 +1,38 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList:
+            return 0
+        
+        adj = collections.defaultdict(list)
+        wordList.append(beginWord)
+        
+        for w in wordList:
+            for j in range(len(w)):
+                pattern = w[:j] + '*' + w[j+1:]
+                adj[pattern].append(w)
+                
+        visit = set([beginWord])
+        q = collections.deque([beginWord])
+        
+        length = 1
+        
+        while q:
+            for i in range(len(q)):
+                w = q.popleft()
+                if w == endWord:
+                    return length
+                else:
+                    for j in range(len(w)):
+                        pattern = w[:j] + '*' + w[j+1:]
+                        for neigh in adj[pattern]:
+                            if neigh not in visit:
+                                visit.add(neigh)
+                                q.append(neigh)
+                        
+            
+            
+            length+=1
+            
+        
+        return 0
