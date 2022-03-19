391ab4c16b1c3bfd33dc84a6d23ad1d1d3c05bb8
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-19 10:37:14 -0700

    Update min-add-to-make-parens-valid.py

--- min-add-to-make-parens-valid.py
+++ min-add-to-make-parens-valid.py
@@ -2,27 +2,28 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         
         # O(n) time O(n) space
+        # Using stack
         
         stack = []
-        
+        res = 0
         for c in s:
-            if c == "(":
+            if c =='(':
                 stack.append(c)
-            elif c == ")": 
+            else:
                 if stack and stack[-1] == "(":
                     stack.pop()
                 else:
                     stack.append(c)
         
         return len(stack)
-        
+
         # O(n) time O(1) space
+        # Without stack
         
-        ans = bal = 0
-        for symbol in s:
-            bal += 1 if symbol == '(' else -1
-            # It is guaranteed bal >= -1
+        res, bal = 0, 0
+        for c in s:
+            bal+=1 if c == '(' else -1
             if bal == -1:
-                ans += 1
-                bal += 1
-        return ans + bal
+                res+=1
+                bal+=1
+        return res + bal
