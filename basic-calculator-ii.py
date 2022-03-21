596997ec7b6cc585dd6630686eea01c7a428d072
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-21 10:05:59 -0700

    Update basic-calculator-ii.py

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -2,34 +2,35 @@ class Solution:
     def calculate(self, s: str) -> int:
         
         # O(n) time O(n) space
+        
         stack = []
-        currNum, currChar, op = 0, '', '+'
+        currDigit, currChar, currOp = 0, '', '+'
         length = len(s)
         
         def div(n1, n2):
             if n1 > 0 or n1 % n2 == 0:
                 return n1 // n2
             return (n1 // n2) + 1
-                
         
         for i in range(length):
             currChar = s[i]
             if currChar.isdigit():
-                currNum = (currNum * 10) + int(currChar)
+                currDigit = (currDigit * 10) + int(currChar)
             if (not currChar.isdigit() and not currChar.isspace()) or i == length-1:
-                if op == '+':
-                    stack.append(currNum)
-                elif op == '-':
-                    stack.append(-currNum)
-                elif op == '*':
-                    stack[-1] = stack[-1] * currNum
+                if currOp == '+':
+                    stack.append(currDigit)
+                elif currOp == '-':
+                    stack.append(-currDigit)
+                elif currOp == '*':
+                    stack[-1] = stack[-1] * currDigit
                 else:
-                    stack[-1] = div(stack[-1], currNum)
-                currNum = 0
-                op = currChar
+                    stack[-1] = div(stack[-1], currDigit)
+                currDigit = 0
+                currOp = currChar
         
         res = 0
         while stack:
             res += stack.pop()
             
         return res
+            
