144d4db6e3488d42bab1e3193765216fd5653cf9
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-27 14:57:34 -0700

    Update lowest-common-ancestor-of-a-binary-tree-iii.py

--- lowest-common-ancestor-of-a-binary-tree-iii.py
+++ lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -11,48 +11,15 @@ class Node:
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         
-        # Approach 1: Use 2 Qs
-        # O(logn) time O(logn) space
+        # O(logn) time O(1) space
+        # 2 pointers
         
-        if p.parent == q.parent:
-            return p.parent
-        if p.parent == q:
-            return q
-        if q.parent == p:
-            return p
-        
-        pAncestors = collections.deque([])
-        qAncestors = collections.deque([])
-        
-        while p:
-            pAncestors.append(p)
-            p = p.parent 
+        pPtr, qPtr = p, q
+        ctr = 0
+        while pPtr != qPtr:
+            ctr+=1
+            pPtr = pPtr.parent if pPtr.parent else q
+            qPtr = qPtr.parent if qPtr.parent else p
             
-        while q:
-            qAncestors.append(q)
-            q = q.parent 
-        
-        while len(pAncestors) > len(qAncestors):
-            pAncestors.popleft()
-            
-        while len(qAncestors) > len(pAncestors):
-            qAncestors.popleft()
-            
-        while len(pAncestors) > 1:
-            if pAncestors[0] == qAncestors[0]:
-                return pAncestors[0]
-            pAncestors.popleft()
-            qAncestors.popleft()
-            
-        return pAncestors[0]
-
-        # Approach 2: No additional space
-        # O(logn) time, O(1) space
-        
-        a, b = p, q
-        
-        while a != b:
-            a = a.parent if a else q
-            b = b.parent if b else p
-            
-        return a
+        print(ctr)
+        return pPtr
