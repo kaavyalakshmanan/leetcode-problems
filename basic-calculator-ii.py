232b01e1faabf22eddac801d8617405366030a76
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-27 20:03:53 -0700

    Update basic-calculator-ii.py

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -8,10 +8,7 @@ class Solution:
         currDigit, currOp = 0, '+'
         
         def div(num1, num2):
-            if num1 > 0 or num1 % num2 == 0:
-                return num1 // num2
-            else:
-                return (num1 // num2) + 1
+            return int(num1 / num2)
         
         for i, c in enumerate(s):
             if c.isdigit():
