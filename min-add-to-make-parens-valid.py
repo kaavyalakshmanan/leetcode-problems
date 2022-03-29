31e2fd631bf1835158ec9cfd224701f1f49516de
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-29 15:18:40 -0700

    Update min-add-to-make-parens-valid.py

--- min-add-to-make-parens-valid.py
+++ min-add-to-make-parens-valid.py
@@ -2,34 +2,28 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         
         # O(n) time O(1) space
-        # Iterate forwards and backwards
-         
+        
         left, right = 0, 0
         count = 0
         
         for i in range(len(s)):
-            currChar = s[i]
-            if currChar == "(":
+            if s[i] == "(":
                 left+=1
             else:
-                right+=1
-            if left == right:
-                left, right = 0, 0
+                right +=1
             if right > left:
-                count += 1
-                right-=1
-        
+                count +=1
+                left, right = 0, 0
+            
         left, right = 0, 0
+        
         for i in range(len(s)-1, -1, -1):
-            currChar = s[i]
-            if currChar == "(":
+            if s[i] == "(":
                 left+=1
             else:
-                right+=1
-            if left == right:
+                right +=1
+            if left > right:
+                count +=1
                 left, right = 0, 0
-            elif left > right:
-                count += 1
-                left-=1
                 
         return count
