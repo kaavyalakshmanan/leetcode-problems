022e6faaa5615d390cfc24cdbf37e35d1db52d37
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-29 15:17:30 -0700

    Update random-pick-with-weight.py

--- random-pick-with-weight.py
+++ random-pick-with-weight.py
@@ -1,37 +1,33 @@
 class Solution:
-    
 
     def __init__(self, w: List[int]):
+        
         # O(n) time O(n) space
+        # Binary Search
         
         self.prefixSums = []
-        cumSum = 0
+        self.totalSum = 0
         for weight in w:
-            cumSum += weight
-            self.prefixSums.append(cumSum)
-            
-        self.totalSum = cumSum
-            
-        
+            self.totalSum += weight
+            self.prefixSums.append(self.totalSum)
         
 
     def pickIndex(self) -> int:
         
         # O(logn) time O(1) space
         
-        target = random.random() * self.totalSum
-        prev = 0
+        target = self.totalSum * random.random()
         l, r = 0, len(self.prefixSums)-1
         
+        
         while l <= r:
-            m = (r+l)//2
-            if target > self.prefixSums[m]:
-                l = m+1
-            elif target < self.prefixSums[m]:
-                r = m-1
+            m = (l+r)//2
+            if self.prefixSums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1 
                 
         return l
-        
         
 
 
