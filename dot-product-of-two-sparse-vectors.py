9a534679a027fc1b018e108f1661a5a231c7898a
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-29 15:16:51 -0700

    Update dot-product-of-two-sparse-vectors.py

--- dot-product-of-two-sparse-vectors.py
+++ dot-product-of-two-sparse-vectors.py
@@ -1,35 +1,16 @@
-class SparseVector:
-    def __init__(self, nums: List[int]):
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
         
-        # O(n) time O(L) space
+        # O(n) time O(1) space
         
-        self.sparseArr = []
-        for i, num in enumerate(nums):
-            if num > 0:
-                self.sparseArr.append((i, num))
-
-    # Return the dotProduct of two sparse vectors
-    def dotProduct(self, vec: 'SparseVector') -> int:
+        output = [len(heights)-1]
+        maxHeight = heights[-1]
         
-        i, j = 0, 0
-        res = 0
-        while i < len(self.sparseArr) and j < len(vec.sparseArr):
-            idxI, valI = self.sparseArr[i]
-            idxJ, valJ = vec.sparseArr[j]
-            if idxI == idxJ:
-                res += (valI * valJ)
-                i+=1
-                j+=1
-            elif idxI < idxJ:
-                i+=1
-            else:
-                j+=1
+        for i in range(len(heights)-2, -1, -1):
+            if heights[i] > maxHeight:
+                maxHeight = heights[i]
+                output.append(i)
                 
-        return res
                 
+        return output[::-1]
         
-
-# Your SparseVector object will be instantiated and called as such:
-# v1 = SparseVector(nums1)
-# v2 = SparseVector(nums2)
-# ans = v1.dotProduct(v2)
