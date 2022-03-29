fb1e251a3cd83be0e4e02932d0034c71290eea19
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-03-29 15:17:52 -0700

    Update minimum-remove-to-make-valid-parens.py

--- minimum-remove-to-make-valid-parens.py
+++ minimum-remove-to-make-valid-parens.py
@@ -1,61 +1,33 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         
-#         # O(n) time O(n) space
-#         # Use stack
+        # O(n) time O(n) space
         
-        stack = []
-        
-        for i, c in enumerate(s):
-            if c == "(":
-                stack.append(i)
-            elif c == ")":
-                if stack and s[stack[-1]] == "(":
-                    stack.pop()
-                else:
-                    stack.append(i)
-                    
-        output = []
-        j = 0
-        for i, c in enumerate(s):
-            if j < len(stack) and i == stack[j]:
-                j+=1
-            else:
-                output.append(c)
-                
-        return "".join(output)
-
-        # O(n) time O(1) space
-        # Pointers
-        
-        forwards, backwards = "", ""
         left, right = 0, 0
+        forwards = ""
         
         for i in range(len(s)):
-            currChar = s[i]
-            if currChar == "(":
+            if s[i] == "(":
                 left+=1
-            elif currChar == ")":
+            elif s[i] == ")":
                 right+=1
-            if right > left:
-                right-=1
+            if right <= left:
+                forwards += s[i]
             else:
-                forwards+=currChar
-                
-        left, right = 0, 0
-        for i in range(len(forwards)-1, -1, -1):
-            currChar = forwards[i]
-            if currChar == "(":
-                left+=1
-            elif currChar == ")":
-                right+=1
-            if left > right:
-                left-=1
-            else:
-                backwards+=currChar
-                
-        return backwards[::-1]
-                
+                left, right, = 0, 0
         
-            
+        left, right = 0, 0 
+        backwards = ""
+        for i in range(len(forwards)-1, -1, -1):
+            if forwards[i] == "(":
+                left+=1
+            elif forwards[i] == ")":
+                right+=1
+            if left <= right:
+                backwards += forwards[i]
+            else:
+                left, right, = 0, 0
+        
+        return backwards[::-1]
+        
                 
