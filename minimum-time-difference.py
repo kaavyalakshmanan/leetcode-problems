c15473159725f6dc7063aa8ce0dce04371c249bc
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-04-15 12:21:59 -0700

    Create minimum-time-difference.py

---
+++ minimum-time-difference.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        
+        # O(nlogn) time O(n) space
+        
+        for i, time in enumerate(timePoints):
+            timeArr = time.split(":")
+            hour, mins = int(timeArr[0]), int(timeArr[1])
+            timePoints[i] = (hour * 60) + mins
+        
+        timePoints.sort()
+        minTimeDiff = inf
+        
+        for i in range(len(timePoints)-1):
+            time1, time2 = timePoints[i], timePoints[i+1]
+            minTimeDiff = min(minTimeDiff, (time2 - time1))
+        
+        minTimeDiff = min(minTimeDiff, (timePoints[0] - timePoints[-1]) % 1440) 
+        
+        return minTimeDiff
+
+
+        
+       
