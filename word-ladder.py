886d9c7a1d0c7eaddf07b8e429f96fb1f3e3d08a
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2022-04-24 19:59:54 -0700

    Update word-ladder.py

--- word-ladder.py
+++ word-ladder.py
@@ -1,41 +1,42 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         
-        # O(n^2 * m) time,  O(n^2 * m) space
-        # BFS Graph
+        # For every word, we want at most 1 char difference
+        # eg - hot
+            # *ot, h*t, ho* --> possibilities 
+        # Build adj list with key = pattern and val = list of words that fall into pattern (eg key = *ot val = [dot, hot])
+        
+        # O(n^2 * m) time O(n^2 * m) space where n = len(wordList) and m = len(word)
         
         if endWord not in wordList:
             return 0
-        
-        adj = collections.defaultdict(list)
+       
+        adj = defaultdict(list)
         wordList.append(beginWord)
         
-        for w in wordList:
-            for j in range(len(w)):
-                pattern = w[:j] + '*' + w[j+1:]
-                adj[pattern].append(w)
-                
-        visit = set([beginWord])
-        q = collections.deque([beginWord])
         
-        length = 1
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + '*' + word[j+1:]
+                adj[pattern].append(word)
+                
+        visit = set(beginWord)
+        q = collections.deque([beginWord])
+        pathLen = 1
         
         while q:
             for i in range(len(q)):
-                w = q.popleft()
-                if w == endWord:
-                    return length
-                else:
-                    for j in range(len(w)):
-                        pattern = w[:j] + '*' + w[j+1:]
-                        for neigh in adj[pattern]:
-                            if neigh not in visit:
-                                visit.add(neigh)
-                                q.append(neigh)
+                word = q.popleft()
+                if word == endWord:
+                    return pathLen
+                for j in range(len(word)):
+                    pattern = word[:j] + '*' + word[j+1:]
+                    for p in adj[pattern]:
+                        if p not in visit:
+                            q.append(p)
+                            visit.add(p)
                         
+            pathLen+=1
             
-            
-            length+=1
-            
-        
         return 0
+            
