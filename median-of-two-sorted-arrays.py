fff7268f5165061d9deb3914e82d98c7eeb19060
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 21:31:39 -0700

    Update median-of-two-sorted-arrays.py

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -1,31 +1,33 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
-        # O(log(m+n)) time O(1) space
+
+        # n = len(A) m = len(B)
+        # O(log(n+m)) time O(1) space
+        # Binary search
         
         A, B = nums1, nums2
-        if len(B) < len(A):
+        if len(A) > len(B):
             A, B = B, A
+
+        left, right = 0, len(A)-1
         total = len(A) + len(B)
-        half = total // 2
-            
-        left, right = 0, len(A)-1 
-        
-        
+        half = total//2
         while True:
-            midA = (left + right) // 2
-            midB = half - midA - 2
+            midA = (right+left)//2
+            midB = half - (midA+1)-1
             leftA, rightA = A[midA] if midA >= 0 else -inf, A[midA+1] if midA+1 < len(A) else inf
             leftB, rightB = B[midB] if midB >= 0 else -inf, B[midB+1] if midB+1 < len(B) else inf
-            
+
             if leftA <= rightB and leftB <= rightA:
-                if (total) % 2 == 1:
+                if total % 2:
                     return min(rightA, rightB)
-                else:
-                    return (max(leftA, leftB) + min(rightA, rightB))/2
-            elif leftB > rightA:
-                left = midA + 1
+                return (min(rightA, rightB) + max(leftA, leftB))/2
+            
+            if leftB > rightA:
+                left = midA+1
             else:
                 right = midA-1
-                
-            
+
+        
+      
+
