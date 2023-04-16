6bdf4c78043b91b0cbf9c8cfc6fee7a00bf03184
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 21:30:45 -0700

    Update regular-expression-matching.py

--- regular-expression-matching.py
+++ regular-expression-matching.py
@@ -1,29 +1,41 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        # O(m*n) time O(m*n) space
-        # Memoization
-        
+        # We are going to use backtracking to solve this
+        # We will memoize with a cache to make more efficient
+        # O(n*m) time O(n*m) space because of memoization
+        # Would have been O(2^n) time if we just did backtracking without memoization
+
+
         cache = {}
-        
-        # Backtracking algo with memoization
-        def findMatch(i, j):
+
+        def backtracking(i, j):
+            # Base case already in cache
             if (i,j) in cache:
                 return cache[(i,j)]
-            if i >= len(s) and j >= len(p):
+
+            # Base case True case
+            if i == len(s) and j == len(p):
                 return True
-            if j >= len(p):
-                return False
             
-            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
-            if j+1 < len(p) and p[j+1] == '*':
-                # Don't use * or use * IF there is match
-                cache[(i,j)] = findMatch(i, j+2) or (match and findMatch(i+1, j))
+            # Base case False case
+            if j == len(p):
+                return False
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+            # * case
+            if j+1 < len(p) and p[j+1] == "*":
+                # Take * if match or don't take *
+                cache[(i,j)] = (match and backtracking(i+1, j)) or backtracking(i, j+2)
                 return cache[(i,j)]
-            if match: 
-                cache[(i,j)] = findMatch(i+1, j+1)
+            # Match case
+            if match:
+                cache[(i,j)] = backtracking(i+1, j+1)
                 return cache[(i,j)]
+            # False case
             else:
-                return False
-            
-        return findMatch(0, 0)
+                cache[(i,j)] = False
+                return cache[(i,j)]
+
+        return backtracking(0, 0)
