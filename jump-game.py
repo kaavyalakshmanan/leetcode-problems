9a2f13af1d771b761042e2cc2705b37d0b1086b8
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 22:00:31 -0700

    Update jump-game.py

--- jump-game.py
+++ jump-game.py
@@ -1,7 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+
+        # O(n) time O(1) space
+        # Greedy algo
+
         
         jumpIdx = len(nums)-1
+
         for i in range(len(nums)-2, -1, -1):
             if nums[i] + i >= jumpIdx:
                 jumpIdx = i
