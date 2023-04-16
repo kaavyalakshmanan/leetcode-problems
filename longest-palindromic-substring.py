fb093c02c6ae81d6499477d6cc5e3b9e279b85ef
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 21:31:16 -0700

    Update longest-palindromic-substring.py

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -3,20 +3,25 @@ class Solution:
         
         # O(n^2) time O(1) space
         # Expand around center 
+
+        maxLen, left, right = 0, 0, 0
         
-        def expandAroundTheCenter(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left, right = left-1, right+1
-            return right-left-1
-        
-        start, end = 0, 0
         for i in range(len(s)):
-            oddLen = expandAroundTheCenter(i, i)
-            evenLen = expandAroundTheCenter(i, i+1)
-            maxLen = max(oddLen, evenLen)
-            
-            if end-start < maxLen:
-                start = i - (maxLen-1)//2
-                end = i + (maxLen//2)
-                
-        return s[start:end+1]
+            evenLen, evenLeft, evenRight = self.findLongest(i, i+1, s)
+            oddLen, oddLeft, oddRight = self.findLongest(i, i, s)
+
+            if evenLen > maxLen and evenLen > oddLen:
+                maxLen = evenLen
+                left, right = evenLeft, evenRight
+            elif oddLen > maxLen and oddLen > evenLen:
+                maxLen = oddLen
+                left, right = oddLeft, oddRight
+
+        return s[left+1:right]
+
+    def findLongest(self, left: int, right: int, s: str) -> (int, int, int):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left-=1
+            right+=1
+        return (right-left-1, left, right)
+        
