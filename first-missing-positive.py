fb07317b8c228715dc425bdb1999219f4dd0526f
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 21:56:13 -0700

    Update first-missing-positive.py

--- first-missing-positive.py
+++ first-missing-positive.py
@@ -1,23 +1,25 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        
+
         # O(n) time O(1) space
-        
-        for i in range(len(nums)):
-            if nums[i] < 0 or nums[i] == 0:
+
+        # Step 1: Replace anything <= 0 with len(nums)+1
+
+        for i, num in enumerate(nums):
+            if num <= 0:
                 nums[i] = len(nums)+1
-        
-        for i in range(len(nums)):
-            currIdx = abs(nums[i])
-            if currIdx > 0 and currIdx <= len(nums) and nums[currIdx-1] > 0:
-                nums[currIdx-1] = nums[currIdx-1] * (-1) 
-            elif currIdx == 0 and currIdx-1 >= 0:
-                nums[currIdx-1] = len(nums)+1
-                
-        for i in range(len(nums)):
-            currNum = nums[i]
-            if currNum > 0:
+
+        # Step 2: Mark array in place
+        for i, num in enumerate(nums):
+            currIdx = abs(num)-1
+            if currIdx < len(nums) and nums[currIdx] > 0:
+                nums[currIdx] *= (-1)
+
+        # Step 3: Find first missing positive 
+        for i, num in enumerate(nums):
+            if num > 0:
                 return i+1
-            
+
         return len(nums)+1
-            
+
+        
