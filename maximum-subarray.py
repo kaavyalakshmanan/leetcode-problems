00ad3430bc5514a0ac99f473b87e8c0674bdc8aa
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 21:59:43 -0700

    Update maximum-subarray.py

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
+
         # O(n) time O(1) space
         # Sliding window
         
         left, right = 0, 0
-        currSum, maxSum = 0, max(nums)
+        res = 0
+        maxRes = -inf
+        
+
         while right < len(nums):
-            currSum += nums[right]
-            maxSum = max(maxSum, currSum)
-            if currSum < 0:
-                currSum = 0
-                left = right+1
+            currSum = res + nums[right]
+            if nums[right] > currSum:
+                res = nums[right]
+                left = right
+            else:
+                res = currSum
+            maxRes = max(maxRes, res)
             right+=1
-            
-        return maxSum
+
+        return maxRes
