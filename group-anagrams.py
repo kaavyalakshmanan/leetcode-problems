104c6f0e11e3122eb171b0aaf415cc78ff956c37
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-15 21:59:01 -0700

    Update group-anagrams.py

--- group-anagrams.py
+++ group-anagrams.py
@@ -1,15 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
+
         # O(n*m) time O(n*m) space
-        # Categorize by count
         
-        res = defaultdict(list)
-        
+        charDict = defaultdict(list)
+
         for s in strs:
             count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')]+=1
-            res[tuple(count)].append(s)
-            
-        return res.values()
+
+            # In python lists cant be keys
+            charDict[tuple(count)].append(s)
+
+
+        return charDict.values()
