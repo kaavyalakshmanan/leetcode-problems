d8995e28801bc39281e445cdbecafab3d95504e9
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-17 13:27:11 -0700

    Update unique-paths.py

--- unique-paths.py
+++ unique-paths.py
@@ -1,19 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        # O(m*n) time O(n) space
+
         # Bottom Up DP
-        
-        if m == 1 and n == 1:
-            return 1
-        
+        # O(m*n) time O(n) space
+
         prevRow = [1] * n
-        prevRow[-1] = 0
+        i = 0
         
-        for i in range(m-2, -1, -1):
+        while i < m-1:
             currRow = [1] * n
-            for j in range(n-2, -1, -1):
+            for j in range(len(currRow)-2, -1, -1):
                 currRow[j] = currRow[j+1] + prevRow[j]
             prevRow = currRow
-            
+            i+=1
+
         return prevRow[0]
