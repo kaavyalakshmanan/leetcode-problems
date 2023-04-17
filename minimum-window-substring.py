66cffb41e9468e46c23181ae990f8a20aa0ed50a
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-17 13:22:20 -0700

    Update minimum-window-substring.py

--- minimum-window-substring.py
+++ minimum-window-substring.py
@@ -1,39 +1,48 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        # O(n) time O(1) space
-        # Sliding window
-        
+
+        # O(n+m) time O(n+m) space
+
         if len(t) > len(s):
             return ""
-        
-        sDict, tDict = {}, {}
+
+        # Step 1; tDict and sDict
+        tDict = {}
+        sDict = {}
         for c in t:
             if c not in tDict:
                 tDict[c] = 1
                 sDict[c] = 0
             else:
                 tDict[c]+=1
-                
-        have, need = 0, len(tDict)
+
+        have, need = len(tDict), 0
         left, right = 0, 0
-        output, length = "", inf
+
+        minLen = inf
+        leftIdx, rightIdx = -1, -1
+
+        # Step 2: iterate over s
         while right < len(s):
-            currChar = s[right]
-            if currChar in tDict:
-                sDict[currChar]+=1
-                if sDict[currChar] == tDict[currChar]:
-                    have+=1
-            while have == need:
-                leftChar = s[left]
-                if right-left+1 < length:
-                    length = right-left+1
-                    output = s[left:right+1]
-                if leftChar in sDict:
-                    sDict[leftChar]-=1
-                    if sDict[leftChar] < tDict[leftChar]:
-                        have-=1
-                left+=1
-            right+=1
-                        
-        return output 
+            if s[right] in sDict:
+                sDict[s[right]]+=1
+
+                if sDict[s[right]] == tDict[s[right]]:
+                    need+=1
+
+                    while have == need:
+                        if right-left+1 < minLen:
+                            minLen = right-left+1
+                            leftIdx = left
+                            rightIdx = right
+                        if s[left] in sDict:
+                            sDict[s[left]]-=1
+
+                            if sDict[s[left]] < tDict[s[left]]:
+                                need-=1
+
+                        left+=1
+
+            right +=1
+
+        return s[leftIdx:rightIdx+1]
