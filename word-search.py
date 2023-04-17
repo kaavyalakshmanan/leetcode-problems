094e40556d36373494440b9a99fe8ca5744b9653
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-17 13:20:10 -0700

    Update word-search.py

--- word-search.py
+++ word-search.py
@@ -1,34 +1,44 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        # O(n*m*3^l) time O(l) space
-        
+
+        # DFS Backtracking
+        # O(n*m*4^l) where n = rows m = cols and l = len(word) time
+        # O(l) space because recurses to length of word
+
+        visited = set()
+        rows, cols = len(board), len(board[0])
+
         def dfs(row, col, idx):
+
             if idx == len(word):
                 return True
-            if row < 0 or row == ROWS or col < 0 or col == COLS:
+
+            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != word[idx] or (row, col) in visited:
                 return False
-            if board[row][col] != word[idx]:
-                return False
-            if (row, col) in visited:
-                return False
-            
+
             visited.add((row, col))
-            match = (
-                dfs(row+1, col, idx+1) or 
-                dfs(row-1, col, idx+1) or
-                dfs(row, col+1, idx+1) or
-                dfs(row, col-1, idx+1)
-            )
-            
+
+            res = dfs(row, col-1, idx+1) or dfs(row, col+1, idx+1) or dfs(row-1, col, idx+1) or dfs(row+1, col, idx+1)
+
             visited.remove((row, col))
-            return match
-        
-        ROWS, COLS = len(board), len(board[0])
-        visited = set()
-        
-        for row in range(ROWS):
-            for col in range(COLS):
+
+            return res
+
+
+        for row in range(rows):
+            for col in range(cols):
                 if dfs(row, col, 0):
                     return True
+
         return False
+
+
+
+            
+
+
+
+            
+
+
+
