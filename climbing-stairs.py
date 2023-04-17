5694036ec0fc868e937997ae99f4a22bc194d4d5
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-17 13:25:27 -0700

    Update climbing-stairs.py

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -1,24 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        # O(n) time O(n) space
-        # Bottom Up DP
-        
-        cache = [1] * (n+1)
-        for i in range(len(cache)-3, -1, -1):
-            cache[i] = cache[i+1] + cache[i+2]
-            
-        return cache[0]
-    
-        # O(n) time O(1) space
-        # Bottom Up DP without extra memory
 
-        first, second = 1, 1
-        i = n-2
-        while i >= 0:
-            curr = first + second
-            first, second = curr, first
-            i-=1
-            
-        return first
+        # O(n) time O(1) space
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        first, second = 2, 1
+        i = 2
+
+        while i < n:
+            temp = first
+            first += second
+            second = temp
+            i+=1
         
+        return first
