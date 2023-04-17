88efef2a2336ad9cb40dcdd99b3d00b5cd150a9c
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-17 13:16:46 -0700

    Update validate-binary-search-tree.py

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -7,25 +7,16 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        # Task: Given a tree, determine if its a valid BST
-        # A valid BST is such that for a given node, its left subtree is strictly smaller and its right subtree is striclty larger in value
-        # No duplicates allowed
-        # Algorithm: The brute force approach would be for every node, check its left subtree and right subtree and determine validity. This is an O(n) operation and we would have to repeat this n times, making it O(n^2)
-        # An optimization is to do DFS and keep track of smallest and largest seen so far
-        # Do that check for every node against smallest and largest
         # O(n) time O(n) space
-        # DFS
+        
+        def validate(node, minVal, maxVal):
 
-        def traverseTree(node: Optional[TreeNode], small: int, large: int):
-            # Base case: True case
             if not node:
                 return True
-            
-            # Base case: False case
-            if node.val <= small or node.val >= large:
+
+            if node.val <= minVal or node.val >= maxVal:
                 return False
 
-            return traverseTree(node.left, small, node.val) and traverseTree(node.right, node.val, large)
+            return validate(node.left, minVal, node.val) and validate(node.right, node.val, maxVal)
 
-        return traverseTree(root, -inf, inf)
-
+        return validate(root, -inf, inf)
