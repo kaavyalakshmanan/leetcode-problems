d6b899e59b45c4395f06b2f664a7541e1661d959
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-18 09:15:05 -0700

    Update maximum-depth-of-binary-tree.py

--- maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree.py
@@ -6,11 +6,32 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
-        # O(n) time O(n) space
-        # Recurse
-        
+
+        # O(n) time O(n) space worst case in case unbalanced tree
+        # BFS
+
         if not root:
             return 0
-        
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        numLevels = 1
+        q = collections.deque([root, None])
+
+        while q:
+            currNode = q.popleft()
+
+            if currNode:
+                if currNode.left:
+                    q.append(currNode.left)
+
+                if currNode.right:
+                    q.append(currNode.right)
+
+            else:
+                if len(q) > 0:
+                    numLevels +=1
+                    q.append(None)
+
+        return numLevels
+
+       
+
