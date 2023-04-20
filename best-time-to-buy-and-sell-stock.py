323200e09404ac5c2954354231a8ec80b83a7c6f
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-20 08:13:49 -0700

    Update best-time-to-buy-and-sell-stock.py

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+
         # O(n) time O(1) space
-        # One pass
-        
-        output, currBuy = 0, prices[0]
-        for i in range(1, len(prices)):
-            output = max(output, prices[i] - currBuy)
-            currBuy = min(currBuy, prices[i])
-        return output 
+
+        res = 0
+        sell = prices[-1]
+
+        for i in range(len(prices)-2, -1, -1):
+            currProfit = sell - prices[i]
+            res = max(res, currProfit)
+
+            sell = max(sell, prices[i])
+
+        return res if res > 0 else 0
+
