4cd765df13227ca03010ee8edc6aa7815a24a40e
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-21 09:04:02 -0700

    Update longest-consecutive-sequence.py

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -1,20 +1,50 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
+
         # O(n) time O(n) space
-        # Use set
-        
-        numSet = set(nums)
-        output = 0
-        
-        for num in nums:
-            curr = 0
-            # Check whether num is the start of a sequence
-            if num-1 not in numSet:
-                while num in numSet:
-                    curr+=1
-                    num+=1
-            output = max(output, curr)
-            
-        return output
+
+        # convert input array into set to make use of O(1) lookup time
+        numsSet = set(nums)
+        start = set()
+        maxLen = 0
+
+        for num in numsSet:
+            # find all the starts for each sequence and add to another set
+            if num-1 not in numsSet:
+                start.add(num)
+
+        for num in start:
+            # find the length of the longest sequence starting at each start
+            currLen = 0
+            i = num
+            while i in numsSet:
+                currLen+=1
+                i+=1
+
+            maxLen = max(maxLen, currLen)
+
+        return maxLen
+
+        # another that doesnt invovle creating 2 separate sets
+
+        numsSet = set(nums)
+        maxLen = 0
+
+        for num in numsSet:
+            end, currLen = num, 1
+            # if we have found a start
+            if num-1 not in numsSet:
+                while end+1 in numsSet:
+                    end+=1
+                    currLen+=1
+            maxLen = max(currLen, maxLen)
+
+        return maxLen
+
                 
+
+
+
+
+        
+
