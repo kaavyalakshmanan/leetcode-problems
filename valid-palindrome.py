0ffd5ea98700527683c294511e7bba24d92b8c45
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-21 09:02:10 -0700

    Update valid-palindrome.py

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,23 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+
         # O(n) time O(1) space
-        # 2 pointer
-        
+        # 2 pointers
+
         left, right = 0, len(s)-1
         while left <= right:
-            leftChar = s[left]
-            rightChar = s[right]
             while left <= right and not s[left].isalnum():
                 left+=1
-            while left <= right and not s[right].isalnum():
+            while right >= left and not s[right].isalnum():
                 right-=1
             if left > right:
                 return True
-            leftChar, rightChar = s[left].lower() if s[left].isalpha() else s[left], s[right].lower() if s[right].isalpha() else s[right]
-            if leftChar != rightChar:
+            if s[left].lower() != s[right].lower():
                 return False
-            left, right = left+1, right-1
-            
+            left+=1
+            right-=1
+
         return True
-            
