f8b419eca0bd538d31ad7d8178289f823de13e6b
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-23 21:27:31 -0700

    Update word-break.py

--- word-break.py
+++ word-break.py
@@ -1,16 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
+
         # O(n^2*m) time O(n) space
-        # Bottom Up DP
-        
-        dp = [False] * (len(s) + 1)
-        dp[-1] = True
-        
-        for i in range(len(dp)-2, -1, -1):
-            for word in wordDict:
-                if i + len(word) < len(dp) and s[i:i+len(word)] == word:
-                    dp[i] = dp[i+ len(word)]
-                    if dp[i] == True:
+
+        dp = [False] * (len(s)+1)
+        dp[len(dp)-1] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)-1) < len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i+len(w)]
+                    if dp[i]:
                         break
+
         return dp[0]
+                
+
