4935181953e491473ce556fc3bed6f923882b6d7
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-23 21:26:08 -0700

    Update linked-list-cycle.py

--- linked-list-cycle.py
+++ linked-list-cycle.py
@@ -6,16 +6,35 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
+
+        # Approach 1: Use a set
+        # O(n) time O(n) space
+
+        nodes = set()
+        ptr = head
+
+        while ptr:
+            if ptr in nodes:
+                return True
+            nodes.add(ptr)
+            ptr = ptr.next
+
+        return False
+
+        # Approach 2: Floyds cycle finding algo without auxiliary space
+
         # O(n) time O(1) space
-        # Fast and slow pointers
-        
-        prehead = ListNode(0, head)
+        prehead = ListNode()
+        prehead.next = head
         slow, fast = prehead, prehead.next
-        
+        ctr = 1
+
         while fast:
-            fast = fast.next.next if fast.next else None
-            slow = slow.next 
             if fast == slow:
                 return True
+            if not ctr % 2:
+                slow = slow.next
+            fast = fast.next
+            ctr+=1
+
         return False
