d351f8fbe4ec9ab6cfa6848cb2a133a3770cd0a8
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-25 09:50:52 -0700

    Update intersection-of-two-linked-lists.py

--- intersection-of-two-linked-lists.py
+++ intersection-of-two-linked-lists.py
@@ -6,49 +6,44 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+
+        # O(n+m) time O(1) space
+
+        if not headA or not headB:
+            return 0
+
+        lenA, lenB = 0, 0
+        pA, pB = headA, headB
+
+        while pA:
+            lenA+=1
+            pA = pA.next
+        while pB:
+            lenB+=1
+            pB = pB.next
+
+        pA, pB = headA, headB
+
+        # Helper function moves corresponding LL pointer so starting at same pos
+        if lenA > lenB:
+            pA = self.getStartingPos(pA, lenA-lenB)
+        elif lenB > lenA:
+            pB = self.getStartingPos(pB, lenB-lenA)
         
-        # O(m+n) time O(1) space
+        while pA and pB:
+            if pA == pB:
+                return pA
+            pA = pA.next
+            pB = pB.next
         
-        m, n = 0, 0
-        ptr1, ptr2 = headA, headB
-        
-        while ptr1:
-            m+=1
-            ptr1 = ptr1.next 
-        ptr1 = headA
-            
-        while ptr2:
-            n+=1
-            ptr2 = ptr2.next 
-        ptr2 = headB
-            
-        if n > m:
-            m, n = n, m
-            ptr1, ptr2 = ptr2, ptr1
-        
-        diff = m-n
-        while diff > 0:
-            if ptr1 == ptr2:
-                return ptr1
-            ptr1 = ptr1.next 
-            diff-=1
-        
-        while ptr1 or ptr2:
-            if ptr1 == ptr2:
-                return ptr1
-            ptr1 = ptr1.next if ptr1 else None
-            ptr2 = ptr2.next if ptr2 else None
-            
         return None
-    
-        # O(m+n) time O(1) space
-        # Further optimized
-        
-        ptr1, ptr2 = headA, headB
-        while ptr1 != ptr2:
-            ptr1 = headB if not ptr1 else ptr1.next 
-            ptr2 = headA if not ptr2 else ptr2.next 
-            
-        return ptr1
-            
-            
+
+    def getStartingPos(self, ptr: ListNode, diff: int) -> Optional[ListNode]:
+
+        while diff > 0:
+            ptr = ptr.next
+            diff-=1
+
+        return ptr
+
+
