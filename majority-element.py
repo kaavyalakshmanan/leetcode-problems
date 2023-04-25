39dc5429efcacad0613831db0ddb5b2dc6750ff7
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-25 09:53:20 -0700

    Update majority-element.py

--- majority-element.py
+++ majority-element.py
@@ -1,36 +1,38 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
+
         # O(n) time O(n) space
-        # HashMap
-        
-        count = {}
-        maxCount, maxVal = 0, 0
-        
+
+        # n = len(nums)//2
+        # count = defaultdict(int)
+
+        # for num in nums:
+        #     count[num]+=1
+        #     if count[num] > n:
+        #         return num
+
+        # now lets solve the followup -- how do we do this in O(1) space?
+        # lets use the boyer-moore voting algo
+        # this is O(n) time O(1) space
+        # the way this algo works:
+            # keep track of a res and count
+            # if currNum == res: increment count
+            # else decrement count
+            # but whenever count == 0 and currNum != res, we need to reset res to currNum and count back to 1
+            # eventually return res 
+
+        count, res = 0, -inf
+
         for num in nums:
-            if num in count:
-                count[num]+=1
-            else:
-                count[num] = 1
-            if count[num] > maxCount:
-                maxCount = count[num]
-                maxVal = num
-            
-        return maxVal 
-    
-        # O(n) time O(1) space
-        # Boyer Moore Algo
-        
-        count, res = 0, 0
-        for num in nums:
-            if count == 0 and num != res:
+            if num != res and count == 0:
                 res = num
                 count+=1
-            elif num == res:
-                count+=1
-            else:
+            elif num != res:
                 count-=1
-                
+            else:
+                count+=1
+
         return res
-            
-            
+
+
+        
