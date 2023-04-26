5b98ff573fd0675649c969f1c0c02710ad508925
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-26 11:44:36 -0700

    Update number-of-islands.py

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,34 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        # O(m*n) time O(m*n) space
-        # BFS
-        
-        ROWS, COLS = len(grid), len(grid[0])
+
+        # O(n*m) time O(n*m) space
+        # DFS
+
+        rows, cols = len(grid), len(grid[0])
         visited = set()
-        numIslands = 0
-        
-        def bfs(row, col):
-            q = collections.deque()
+        res = 0
+
+        def dfs(row, col):
+            if row < 0 or row == rows or col < 0 or col == cols:
+                return 
+            if grid[row][col] == "0":
+                return
+            if (row, col) in visited:
+                return
+
             visited.add((row, col))
-            q.append((row, col))
-            
-            while q:
-                r, c = q.popleft()
-                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-                
-                for dr, dc in directions:
-                    newRow, newCol = r+ dr, c + dc
-                    
-                    if newRow in range(ROWS) and newCol in range(COLS) and grid[newRow][newCol] == "1" and (newRow, newCol) not in visited:
-                        q.append((newRow, newCol))
-                        visited.add((newRow, newCol))
-                        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r, c)
-                    numIslands+=1
-                    
-        return numIslands
-                
+            dfs(row+1, col)
+            dfs(row-1, col)
+            dfs(row, col+1)
+            dfs(row, col-1)
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1" and (row, col) not in visited:
+                    dfs(row, col)
+                    res +=1
+
+        return res
