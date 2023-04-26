7be2e398cf66fb16934beff95a869167d6574d36
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-26 11:47:22 -0700

    Update rotate-array.py

--- rotate-array.py
+++ rotate-array.py
@@ -3,27 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        # O(n) time O(1) space
-        
+
+        # in this problem, we are given an array and an int k
+        # we are tasked with rotating the array so that the ith idx is now
+        # in the i+k index
+
+        # we could obviously solve this by using extra space, but can we solve this
+        # in O(1) space?
+        # yes, by reversing the array, then reversing the first k elements, and the remaining n-k elements
+
+        # step 1: mod k by length of input
+        # we do this because k could be larger than the array input and we would go out of bounds
+        # this way if k == len(nums), we are rotating array 0 times
         k = k % len(nums)
-        
-        def reverse(arr):
-            left, right = 0, len(arr)-1
-            while left < right:
-                arr[left], arr[right] = arr[right], arr[left]
-                left+=1
-                right-=1
-            return arr
-        
-        # Step 1: reverse array
-        reverse(nums)
-        mid = k
-        
-        # Step 2: Reverse first k elements, then reverse remaining
-        nums[:mid] = reverse(nums[:mid]) 
-        
-        # Step 3: Reverse remaining elements
-        nums[mid:] = reverse(nums[mid:])
-            
+
+        # step 2: reverse input array
+        self.reverseArray(nums, 0, len(nums)-1)
+
+        # step 3: reverse the first k elements
+        self.reverseArray(nums, 0, k-1)
+
+        # step 4: reverse the remaining elements
+        self.reverseArray(nums, k, len(nums)-1)
+
+        # O(n) time O(1) space
+
+    def reverseArray(self, nums: List[int], l: int, r: int) -> None:
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l+=1
+            r-=1
+
         
