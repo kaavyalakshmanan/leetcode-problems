9d9680507beeeede9903fca70f6ebf68e326914c
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-04-26 11:46:00 -0700

    Update house-robber.py

--- house-robber.py
+++ house-robber.py
@@ -1,42 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        # O(n) time O(n) space
-        # Bottom Up DP
-        
-        if len(nums) == 1:
-            return nums[0]
-        
-        dp = [0] * len(nums)
-        dp[-1] = nums[-1]
-        dp[-2] = nums[-2]
-        
-        maxAmt = max(dp[-1], dp[-2])
-        
-        for i in range(len(nums)-3, -1, -1):
-            second = nums[i] + dp[i+2] 
-            third = nums[i] + dp[i+3] if i+3 < len(nums) else 0
-            dp[i] = max(second, third)
-            maxAmt = max(maxAmt, dp[i])
-        
-        return maxAmt
 
         # O(n) time O(1) space
-        # Bottom Up DP w/ Space Optimization
-        
-        if len(nums) == 1:
-            return nums[0]
-        
-        maxAmount = max(nums[-1], nums[-2])
-        for i in range(len(nums)-3, -1, -1):
-            second = nums[i] + nums[i+2] 
-            third = nums[i] + nums[i+3] if i+3 < len(nums) else 0
-            nums[i] = max(second, third)
-            maxAmount = max(maxAmount, nums[i])
-        
-        return maxAmount
-            
-            
-    
-    
-    
+        # this is a classic dynamic programming question, how do we recognize that?
+        # in order to figure out the max amt we can rob at idx i, we need to figure out the max we can rob at idx i-1 and i-2
+            # aka, since we cant rob adjacent houses, we either continue to rob from idx i-1, meaning we dont take i, or we disregard i-1 and add i-2 to i
+
+        first, second = 0, 0
+        for num in nums:
+            curr = max(num + first, second)
+            first = second
+            second = curr
+
+        return second
