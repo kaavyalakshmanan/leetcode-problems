0ab4999fa34b750bf59b303f5f87f2702d535daf
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-03 10:05:33 -0700

    Update insert-delete-get-random-o1.py

--- insert-delete-get-random-o1.py
+++ insert-delete-get-random-o1.py
@@ -1,38 +1,38 @@
 class RandomizedSet:
 
+    # O(1) time for everything
+
     def __init__(self):
-        
-        # O(1) time O(n) space
-        
+
+        # map val to idx in arr
         self.dict = {}
-        self.nums = []
+        self.arr = []
         
 
     def insert(self, val: int) -> bool:
-        
         if val not in self.dict:
-            self.dict[val] = len(self.nums)
-            self.nums.append(val)
+            self.arr.append(val)
+            self.dict[val] = len(self.arr)-1
             return True
         return False
         
 
     def remove(self, val: int) -> bool:
+
+        if val not in self.dict:
+            return False
+        idx = self.dict[val]
+        self.arr[idx] = self.arr[-1]
+        self.dict[self.arr[idx]] = idx
+        self.arr.pop()
+        del self.dict[val]
         
-        if val in self.dict:
-            idx = self.dict[val]
-            lastVal = self.nums[-1]
-            self.nums[idx] = lastVal
-            self.nums.pop()
-            self.dict[lastVal] = idx
-            del self.dict[val]
-            return True
-        return False
+        return True
         
 
     def getRandom(self) -> int:
-        
-        return random.choice(self.nums)
+        idx = randrange(len(self.arr))
+        return self.arr[idx]
         
 
 
