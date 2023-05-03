3e01937d47f75dc3c9a1e2ba80dd5debbdf8b485
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-03 10:01:07 -0700

    Update find-all-anagrams-in-a-string.py

--- find-all-anagrams-in-a-string.py
+++ find-all-anagrams-in-a-string.py
@@ -1,41 +1,42 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        
+
         # O(n) time O(m) space
-        # Sliding window
+        # sliding window
         
-        if len(s) < len(p):
-            return []
-        
-        pDict, sDict = {}, {}
-        output = []
+        res = []
+
+        if len(p) > len(s):
+            return res
+
+        sDict, pDict = {}, {}
         for c in p:
             pDict[c] = 1 + pDict.get(c, 0)
             sDict[c] = 0 + sDict.get(c, 0)
-            
-        have, need = 0, len(pDict)
+
         left, right = 0, 0
-        
-        # Move right ptr to correct window length
+        have, need = 0, len(pDict)
+
         while right-left+1 < len(p):
             if s[right] in sDict:
                 sDict[s[right]]+=1
-                if pDict[s[right]] == sDict[s[right]]:
+                if sDict[s[right]] == pDict[s[right]]:
                     have+=1
             right+=1
-                  
+
         while right < len(s):
             if s[right] in sDict:
                 sDict[s[right]]+=1
-                if pDict[s[right]] == sDict[s[right]]:
+                if sDict[s[right]] == pDict[s[right]]:
                     have+=1
                 if have == need:
-                    output.append(left)
+                    res.append(left)
             if s[left] in sDict:
                 if sDict[s[left]] == pDict[s[left]]:
                     have-=1
                 sDict[s[left]]-=1
-            left+=1
+                
             right+=1
-            
-        return output
+            left+=1
+
+        return res
