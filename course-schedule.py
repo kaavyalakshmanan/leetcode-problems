44fe6bbb70301511e76a517aa5aadb84eb1dad4c
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-14 10:43:24 -0700

    Update course-schedule.py

--- course-schedule.py
+++ course-schedule.py
@@ -1,35 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+
+        # classic dfs graph problem
         # O(n+p) time O(n) space
-        
-        preMap = { i: [] for i in range(numCourses) }
-        visited = set()
-        
-        # Initialize preMap
-        for course, prereq in prerequisites:
-            preMap[course].append(prereq)
-                
-        # Check using DFS
-        def dfs(course):
-            if course in visited:
+        # step 1: populate adjacency list
+        adj = { i: [] for i in range(numCourses) }
+
+        for c1, c2 in prerequisites:
+            adj[c1].append(c2)
+
+        visit = set()
+
+        # step 2: dfs algo
+        def dfs(c):
+            if c in visit:
                 return False
-            if preMap[course] == []:
+            if not adj[c]:
                 return True
-            
-            visited.add(course)
-            for p in preMap[course]:
+
+            visit.add(c)
+            for p in adj[c]:
                 if not dfs(p):
                     return False
-            visited.remove(course)
-            preMap[course] = []
-            
+
+            visit.remove(c)
+            adj[c] = []
             return True
-        
-        
-        for course, p in prerequisites:
-            if not dfs(course):
+
+        for k, v in adj.items():
+            if not dfs(k):
                 return False
-        
+
         return True
-                
