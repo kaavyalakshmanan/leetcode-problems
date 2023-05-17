56b23aa343fe9b60f0015b7ff88b255bd025017c
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-17 09:18:47 -0700

    Update basic-calculator-ii.py

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        
+
         # O(n) time O(n) space
-        
+
         stack = []
         currDigit, currOp = 0, '+'
-        
+
         for i, c in enumerate(s):
             if c.isdigit():
                 currDigit = (currDigit * 10) + int(c)
@@ -14,15 +14,19 @@ class Solution:
                     stack.append(currDigit)
                 elif currOp == '-':
                     stack.append(-currDigit)
-                elif currOp == '*':
-                    stack[-1]*=currDigit
+                elif currOp == "*":
+                    stack[-1] = stack[-1] * currDigit
                 else:
                     stack[-1] = int(stack[-1]/currDigit)
                 currOp = c
                 currDigit = 0
-                    
+
         res = 0
         while stack:
             res += stack.pop()
-            
         return res
+
+
+
+                
+
