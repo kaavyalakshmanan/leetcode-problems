4e5211d7014b2adbabffd7e84dcbcb967aaccd8b
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-17 09:19:44 -0700

    Update kth-smallest-element-in-a-bst.py

--- kth-smallest-element-in-a-bst.py
+++ kth-smallest-element-in-a-bst.py
@@ -6,44 +6,19 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        # Approach 1: Use array and sort
-        # O(nlogn) time O(n) space
-        
-        def helper(node):
+
+        # O(n) time O(n) space
+
+        # left, root, right
+        stack = []
+
+        def traverse(node):
             if not node:
                 return
-            res.append(node.val)
-            helper(node.left)
-            helper(node.right)
-        
-        res = []
-        helper(root)
-        res.sort()
-        return res[k-1]
-    
-        # Approach 2: Iterative Inorder Traversal
-        # O(n) time O(n) space for unbalanced tree
-        # O(logn) time O(logn) space for balanced tree
-        
-        n = 0
-        stack = []
-        curr = root
-        
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()
-            n+=1
-            if n == k:
-                return curr.val
-            curr = curr.right
-                
-            
-        
-                    
 
+            traverse(node.left)
+            stack.append(node.val)
+            traverse(node.right)
 
-            
-        
+        traverse(root)
+        return stack[k-1]
