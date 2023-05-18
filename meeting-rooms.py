fed5bba7464079a1a32b4b7550d71e8c6f1b8b9c
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-18 08:38:30 -0700

    Update meeting-rooms.py

--- meeting-rooms.py
+++ meeting-rooms.py
@@ -1,19 +1,19 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        
+
+        # O(nlogn) time O(n) space
+
         if not intervals:
             return True
-        
+
         intervals.sort()
-        s1, e1 = intervals[0]
-        i = 1
-        
-        while i < len(intervals):
-            s2, e2 = intervals[i]
-            if s2 < e1:
+        prevStart, prevEnd = intervals[0][0], intervals[0][1]
+
+        for start, end in intervals[1:]:
+            if start >= prevStart and start < prevEnd:
                 return False
-            s1, e1 = s2, e2
-            i+=1
-        
+            prevStart, prevEnd = start, end
+
         return True
-        
+
+
