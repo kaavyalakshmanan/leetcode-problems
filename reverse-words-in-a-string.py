64db6a9bef3853c322312168466c03ebd6415014
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-05-22 19:59:19 -0700

    Update reverse-words-in-a-string.py

--- reverse-words-in-a-string.py
+++ reverse-words-in-a-string.py
@@ -1,21 +1,16 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
+
         # O(n) time O(n) space
-        
-        sArray = s.split(" ")
-        sArray = sArray[::-1]
-        i = len(sArray)-1
-        while i >= 0 and sArray[i] == "":
-            i-=1
-        sArray = sArray[:i+1]
-            
-        res = ""
-        for i, word in enumerate(sArray):
-            if word == "":
-                continue
-            res += word
-            if i < len(sArray)-1:
-                res+=" "
-        
-        return res
+
+        res = []
+
+        sArr = s.split(" ")
+
+        for word in sArr:
+            if word != "":
+                res.append(word)
+
+        res = res[::-1]]
+
+        return " ".join(res)
