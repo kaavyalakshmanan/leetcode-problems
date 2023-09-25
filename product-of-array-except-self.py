30c6d373ceaa49b197b1a0f9a3efd0e397cb7343
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-09-25 14:38:47 -0700

    Update product-of-array-except-self.py

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -1,23 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        # in order to get the product of everything ezxcept self in O(n) time and O(1) space, we need to compute the left product and the right product
-            # aka, the product of everything except self when we iterate from the left
-            # and then the product of everything except self when we interate from the right
-            # then at an arbitrary index, we would multiply the left and right
-            # which would give us total product of everything except self
+        # O(n) time O(n) space
+        # Forwards & backwards
 
-            # O(n) time O(1) space
+        answer = [1] * len(nums)
+        prefix, postfix = nums[0], 1
 
-        prefix, postfix = 1, 1
-        res = [1] * len(nums)
+        for i in range(1, len(nums)):
+            answer[i] = prefix
+            prefix*= nums[i]
 
-        for i in range(len(nums)):
-            res[i] = prefix 
-            prefix *= nums[i]
+        for i in range(len(nums)-2, -1, -1):
+            postfix*= nums[i+1]
+            answer[i] *= postfix
 
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-
-        return res
+        return answer
+        
