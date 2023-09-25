9d1f2a266e27306c491d1b40f145753b86395d1e
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-09-25 14:27:48 -0700

    Update contains-duplicates.py

--- contains-duplicates.py
+++ contains-duplicates.py
@@ -2,16 +2,14 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
         # O(n) time O(n) space
-        # Use a hashmap
-
-        if len(nums) == 1:
-            return False
-
+        # Count dict
+        
         count = defaultdict(int)
-
         for num in nums:
             count[num]+=1
-            if count[num] == 2:
+        
+        for k, v in count.items():
+            if v >= 2:
                 return True
 
         return False
