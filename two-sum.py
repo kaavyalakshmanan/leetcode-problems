3f701496e3e747ad28a64e60e339afe84090eec5
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-09-25 14:29:17 -0700

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -2,13 +2,15 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         # O(n) time O(n) space
-        # Hashmap
-        
+        # HashMap
+
         compDict = {}
 
         for i in range(len(nums)):
-            comp = target - nums[i]
+            comp = target-nums[i]
             if comp not in compDict:
                 compDict[nums[i]] = i
             else:
                 return [compDict[comp], i]
+
+        
