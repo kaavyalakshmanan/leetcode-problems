904c3634524840c61d863cf03ed434fe8329b640
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-09-29 15:13:04 -0700

    Update group-anagrams.py

--- group-anagrams.py
+++ group-anagrams.py
@@ -2,14 +2,15 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         # O(n*m) time O(n*m) space
-        # Frequency counter of chars
-        
+        # Frequency count
+
         res = defaultdict(list)
 
         for w in strs:
-            chars = [0] * 26
+            count = [0] * 26
             for c in w:
-                chars[ord(c) - ord('a')]+=1
-            res[tuple(chars)].append(w)
+                count[ord(c) - ord('a')]+=1
+            res[tuple(count)].append(w)
 
         return res.values()
+        
