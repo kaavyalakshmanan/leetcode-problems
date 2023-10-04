ca7cc57ba3f57aa191d60ce41441319cc3771bbb
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-10-04 10:08:24 -0700

    Update trapping-rainwater.py

--- trapping-rainwater.py
+++ trapping-rainwater.py
@@ -1,25 +1,28 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        
+        # Algo: 2 pointer
+        # keep track of maxLeft and maxRight
+        # current val = min(left, right)
+        # current amount trapped = min(maxLeft, maxRight) - current val
+        # move the pointer pointing to smaller val
 
         # O(n) time O(1) space
 
         left, right = 0, len(height)-1
-        leftMax, rightMax = 0, 0
+        maxLeft, maxRight = height[left], height[right]
         res = 0
 
         while left < right:
-            leftMax = max(leftMax, height[left])
-            rightMax = max(rightMax, height[right])
-            currRes = min(leftMax, rightMax) - min(height[left], height[right])
-            if currRes > 0:
-                res += currRes
-            
-            if leftMax <= rightMax:
+            currentVal = min(height[left], height[right])
+            currentAmt = min(maxLeft, maxRight) - currentVal
+            if currentAmt > 0:
+                res += currentAmt
+            if height[left] <= height[right]:
                 left+=1
+                maxLeft = max(maxLeft, height[left])
             else:
                 right-=1
+                maxRight = max(maxRight, height[right])
 
         return res
-
-
-        
