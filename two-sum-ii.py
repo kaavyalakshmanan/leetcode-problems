ca27a726836a6ce61454eac8252bfe41aea7d8fe
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-10-04 10:05:35 -0700

    Update two-sum-ii.py

--- two-sum-ii.py
+++ two-sum-ii.py
@@ -2,14 +2,14 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
 
         # O(n) time O(1) space
-
+        # 2 pointers
+        
         left, right = 0, len(numbers)-1
         while left < right:
-            currSum = numbers[left] + numbers[right]
-            if currSum == target:
+            if numbers[left] + numbers[right] == target:
                 return [left+1, right+1]
-            if currSum > target:
-                right-=1
-            else:
+            if numbers[left] + numbers[right] < target:
                 left+=1
+            else:
+                right-=1
             
