c9e579353598f5017c759603976af2601835191f
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-10-11 15:10:32 -0700

    Update sliding-window-maximum.py

--- sliding-window-maximum.py
+++ sliding-window-maximum.py
@@ -1,43 +1,24 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        
-        # Approach 1: Compute max for every sliding window -- TLE
-        # O(n*k) time O(n) space
-        
-#         start, end = 0, k-1
-#         output = []
-        
-#         while end < len(nums):
-#             currMax = max(nums[start:end+1])
-#             output.append(currMax)
-#             end+=1
-#             start+=1
-            
-            
-#         return output
-    
-        # Approach 2: Monotoniclly Decreasing Q
-        
-        output = []
-        q = collections.deque() # Contains indices
-        left, right = 0, 0
-        
+
+        res = []
+        left = right = 0
+        q = collections.deque() # contains indices
+
         while right < len(nums):
-            # Pop smaller values from q
+            # remove smaller values in q so q remains monotonically decreasing
             while q and nums[q[-1]] < nums[right]:
                 q.pop()
             q.append(right)
-            
-            # Remove left from q if out of bounds
+
+            # if left val of q is no longer in window remove it
             if left > q[0]:
                 q.popleft()
-                
-            if (right + 1) >= k:
-                output.append(nums[q[0]])
+
+            if right+1 >= k:
+                res.append(nums[q[0]])
                 left+=1
-                
+
             right+=1
-            
-        return output 
-            
-            
+
+        return res
