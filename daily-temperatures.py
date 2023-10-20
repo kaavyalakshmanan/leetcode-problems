1f201b7bbd5a4326f7761da2acfdd3747c677cfa
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2023-10-20 16:15:02 -0700

    Update daily-temperatures.py

--- daily-temperatures.py
+++ daily-temperatures.py
@@ -1,22 +1,19 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
+
         # O(n) time O(n) space
-        # Use a stack
-        
-        output = [0] * (len(temperatures))
-        stack = [(len(temperatures)-1, temperatures[-1])]
-        
-        
+
+        res = [0] * len(temperatures) 
+        stack = [[temperatures[-1], len(temperatures)-1]]
+
         for i in range(len(temperatures)-2, -1, -1):
-            prevIdx, prevTemp = stack[-1]
-            while temperatures[i] >= prevTemp:
+            # Stack has to remain monotonically decreasing
+            while stack and stack[-1][0] <= temperatures[i]:
                 stack.pop()
-                if not stack:
-                    break
-                prevIdx, prevTemp = stack[-1]
+
             if stack:
-                output[i] = prevIdx - i
-            stack.append((i, temperatures[i]))
-            
-        return output
+                res[i] = stack[-1][1] - i
+            stack.append([temperatures[i], i])
+
+        return res
+        
