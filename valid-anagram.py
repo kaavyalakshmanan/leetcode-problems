80e834ac004c7d87b5a06987dddac19f3fe063e6
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-01-02 10:28:18 -0800

    Update valid-anagram.py

--- valid-anagram.py
+++ valid-anagram.py
@@ -1,19 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        # O(n) time O(1) space
-        # Frequency count
+        # O(n) time O(n) space
+        # Use char count
         
         if len(s) != len(t):
             return False
 
         count = [0] * 26
-        for i, c in enumerate(s):
-            count[ord(c) - ord('a')]+=1
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')]+=1
             count[ord(t[i]) - ord('a')]-=1
-        
+
         for c in count:
             if c != 0:
                 return False
 
         return True
+
