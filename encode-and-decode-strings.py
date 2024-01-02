f658a13b9df755c22aba642118bfe834d6edcb0b
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-01-02 10:29:50 -0800

    Update encode-and-decode-strings.py

--- encode-and-decode-strings.py
+++ encode-and-decode-strings.py
@@ -1,14 +1,19 @@
 class Codec:
+
+    # O(n) time O(n) space
+    # Iterate over string
+    
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
 
-        # O(n) time O(n) space
-
         res = ""
+
         for i, s in enumerate(strs):
-            res += s
-            res += "£"
+            res+=s
+            if i < len(strs)-1:
+                res+="£"
+            
 
         return res
         
@@ -17,13 +22,8 @@ class Codec:
         """Decodes a single string to a list of strings.
         """
 
-        # O(n) time O(n) space
-
-        res = s.split("£")
-        # if res[-1] == "":
-        #     res = res[:-1]
-
-        return res[:-1]
+        sArr = s.split("£")
+        return sArr
         
 
 
