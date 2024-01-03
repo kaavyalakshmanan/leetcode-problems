b06398a96a6aedfe95bf17f5aa87d43938d4fb8a
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-01-03 11:11:47 -0800

    Update 3-sum.py

--- 3-sum.py
+++ 3-sum.py
@@ -2,25 +2,27 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
         # O(n^2) time O(n) space
-        # Reeduce to Two Sum II
+        # 2 pointers
         
         nums.sort()
         res = []
 
-        for i, num in enumerate(nums):
-            if i > 0 and num == nums[i-1]:
+        for i, a in enumerate(nums):
+            if i > 0 and nums[i-1] == a:
                 continue
-            j, k = i+1, len(nums)-1
-            while j < k:
-                threeSum = num + nums[j] + nums[k]
-                if threeSum < 0:
-                    j+=1
-                elif threeSum > 0:
-                    k-=1
+            left, right = i+1, len(nums)-1
+            while left < right:
+                threeSum = a + nums[left] + nums[right]
+                if threeSum > 0:
+                    right-=1
+                elif threeSum < 0:
+                    left+=1
                 else:
-                    res.append([num, nums[j], nums[k]])
-                    j+=1
-                    while j < k and nums[j] == nums[j-1]:
-                        j+=1
+                    res.append([a, nums[left], nums[right]])
+                    left+=1
+                    right-=1
+
+                    while left < right and nums[left] == nums[left-1]:
+                        left+=1
 
         return res
