6defd129252c51d11284258e3b42a03ca2ba1d50
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-01-03 11:13:29 -0800

    Update longest-substring-without-repeating-characters.py

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -4,21 +4,16 @@ class Solution:
         # O(n) time O(n) space
         # Sliding window
         
-        res = 0
-        currLen = 0
         left, right = 0, 0
-        pos = {}
+        res = 0
+        sDict = {}
 
         while right < len(s):
-            if s[right] not in pos:
-                currLen+=1
-                pos[s[right]] = right
-            else:
-                res = max(res, currLen)
-                if pos[s[right]] >= left:
-                    left = pos[s[right]]+1
-                currLen = (right-left+1)
-                pos[s[right]] = right
+            if s[right] in sDict and sDict[s[right]] >= left:
+                res = max(res, right-left)
+                left = sDict[s[right]]+1
+            sDict[s[right]] = right
             right+=1
 
-        return max(res, currLen)
+        res = max(res, right-left)
+        return res
