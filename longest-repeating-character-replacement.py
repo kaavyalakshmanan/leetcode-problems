e099535246b4762dbd2bce4a29fae03677579c5f
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-01-05 10:41:55 -0800

    Update longest-repeating-character-replacement.py

--- longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement.py
@@ -1,24 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
-        # O(n) time O(n) space
+        # O(n) time O(1) space bc at most 26 chars
         # Sliding window
         
-        left, right = 0, 0
         count = defaultdict(int)
-        maxCount = 0
         res, currLen = 0, 0
+        left, right = 0, 0
 
         while right < len(s):
             count[s[right]]+=1
-            maxCount = max(maxCount, count[s[right]])
-            if (right-left+1) - maxCount <= k:
-                res = max(res, right-left+1)
+            currLen = max(currLen, count[s[right]])
+            if (right-left+1) - currLen > k:
+                count[s[left]]-=1
+                left+=1
+                currLen = max(count.values())
+                res = max(res, right-left)
             else:
-                while (right-left+1) - maxCount > k:
-                    count[s[left]]-=1
-                    left+=1
-                    maxCount = max(count.values())
+                res = max(res, right-left+1)
             right+=1
 
         return res
+                
+
+        
