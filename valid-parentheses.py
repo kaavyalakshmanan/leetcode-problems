e4acb50bb8f5c54815a699690c4e7cf83f55c438
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-11-24 11:10:17 -0800

    Update valid-parentheses.py

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -2,24 +2,17 @@ class Solution:
     def isValid(self, s: str) -> bool:
 
         # O(n) time O(n) space
+        # Use a stack
         
-        # Stack will keep track of open brackets seen so far
-        # We pop from stack when we see corresponding close bracket 
         stack = []
-
+        
         for c in s:
-            # Open bracket case
-            if c in {'(', '[', '{'}:
+            if c == '(' or c == '[' or c == '{':
                 stack.append(c)
-            # Close bracket case
+            elif len(stack) == 0 or (c == ')' and stack[-1] != '(') or (c == ']' and stack[-1] != '[') or (c == '}' and stack[-1] != '{'):
+                return False
             else:
-                # Haven't seen a corresponding open bracket
-                if len(stack) == 0:
-                    return False
-                # Mismatch case
-                if (c == ')' and stack[-1] != "(") or (c == ']' and stack[-1] != "[") or (c == '}' and stack[-1] != "{"):
-                    return False
-
                 stack.pop()
 
         return len(stack) == 0
+            
