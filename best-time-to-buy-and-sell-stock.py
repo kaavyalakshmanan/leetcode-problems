0b8d644f4e35089cf2e027a2ef0c1c3d45f41f39
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-11-24 11:16:49 -0800

    Update best-time-to-buy-and-sell-stock.py

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -2,12 +2,21 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         # O(n) time O(1) space
-        # Sliding window
+        # 2 pointers
         
-        currSell = prices[0]
-        res = 0
-        for i in range(1, len(prices)):
-            res = max(res, prices[i] - currSell)
-            currSell = min(currSell, prices[i])
+        minSoFar = prices[0]
+        maxProfit = 0
 
-        return res
+        left, right = 0, 1
+        while right < len(prices):
+            maxProfit = max(maxProfit, prices[right] - prices[left])
+            minSoFar = min(minSoFar, prices[right])
+            left = right if minSoFar < prices[left] else left
+            if (prices[right] < minSoFar):
+                left = right
+                minSoFar = prices[right]
+            right+=1
+
+        return maxProfit
+
+        return maxProfit
