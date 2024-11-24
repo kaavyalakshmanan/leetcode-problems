0f84d2a25f70e7505bc0ecd0041b08429fd98383
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2024-11-24 11:19:30 -0800

    Update product-of-array-except-self.py

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -1,19 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        # O(n) time O(n) space
-        # Arrays
+        # O(n) time O(n) space if we count res arr
+        # Iterate forwards & backwards
         
         res = [1] * len(nums)
-        temp = 1
+        productSoFar = 1
 
-        for i in range(len(nums)):
-            res[i] *= temp
-            temp *= nums[i]
-
-        temp = 1
+        # Step 1: iterate backwards
         for i in range(len(nums)-1, -1, -1):
-            res[i] *= temp
-            temp *= nums[i]
+            res[i] = productSoFar
+            productSoFar *= nums[i]
+
+        # Step 2: Iterate forwards
+        productSoFar = 1
+        for i in range(len(nums)):
+            res[i] *= productSoFar
+            productSoFar *= nums[i]
 
         return res
